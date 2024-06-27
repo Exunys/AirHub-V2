e2e48ff071c4f1ba13242a8ce228804d905efab3
Aleksandar <aleksandar.exunys@gmail.com>
2024-06-27 04:10:02 +0200

    Update Main.lua

--- src/Main.lua
+++ src/Main.lua
@@ -1,19 +1,30 @@
 local game = game
 local select, pcall, loadstring, warn, getgenv = select, pcall, loadstring, warn, getgenv
 
-local Success, Unload = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/Modules/Original.lua")))) -- Original / best version
+local __index = getrawmetatable(game).__index
 
-if not Success then
-	warn("AIRHUB_V2 > Loader - Your script execution software does not support the original / best version, trying the degraded version...")
-
-	getgenv().AirHubV2Loaded = nil -- Will implement a better solution in the future...
-
-	if Unload then
-		Unload()
-	end
+if __index(game.Players, "LocalPlayer") then
+	local Success, Unload = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/Modules/Original.lua")))) -- Original / best version
 	
-	Success = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/Modules/Primitive.lua")))) -- Degraded for UWP (works on web version)
+	if not Success then
+		warn("AIRHUB_V2 > Loader - Your script execution software does not support the original / best version, trying the degraded version...")
+	
+		getgenv().AirHubV2Loaded = nil -- Will implement a better solution in the future...
+	
+		if Unload then
+			Unload()
+		end
+		
+		Success = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/Modules/Primitive.lua")))) -- Degraded for UWP (works on web version)
+	
+		if not Success then
+			return warn("AIRHUB_V2 > Loader - Your script execution software does not support this script.")
+		end
+	end
 
+else
+	Success = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/Modules/Primitive.lua")))) -- Degraded for UWP (works on web version)
+	
 	if not Success then
 		return warn("AIRHUB_V2 > Loader - Your script execution software does not support this script.")
 	end
